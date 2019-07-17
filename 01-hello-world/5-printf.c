1075a93ed5e6869e488e5b517dde697b280b0e3d
CrisOspina <crisospina523@gmail.com>
2019-07-17 19:16:41 +0000

    printf

---
+++ 01-hello-world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* printf 
+   gcc -Wall 5-printf.c 
+   ./a.out
+ */
+
+int main(void){
+ printf("With proper grammar, but the outcome is a piece of art \n");
+ return (0);
+}
+
